0782bc49ae9d92e66ff13a344e55f3b6384f4c0c
Mat Pataki <matpataki@gmail.com>
2017-03-29 14:25:40 -0400

    add esc_deploy script

---
+++ esc_deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BUILD_NUMBER=$1
+SERVICE_NAME="flask-signup-service"
+IMAGE_VERSION="v_"${BUILD_NUMBER}
+TASK_FAMILY="flask-signup"
+
+# Create a new task definition for this build
+sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" flask-signup.json > flask-signup-v_${BUILD_NUMBER}.json
+aws ecs register-task-definition --family flask-signup --cli-input-json file://flask-signup-v_${BUILD_NUMBER}.json
+
+# Update the service with the new task definition and desired count
+TASK_REVISION=`aws ecs describe-task-definition --task-definition flask-signup | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
+if [ ${DESIRED_COUNT} = "0" ]; then
+    DESIRED_COUNT="1"
+fi
+
+aws ecs update-service --cluster default --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count ${DESIRED_COUNT}
