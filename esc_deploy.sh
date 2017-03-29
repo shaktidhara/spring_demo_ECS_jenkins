9978f5ca345649bd4369b3dda986f4318ce4352d
Mat Pataki <matpataki@gmail.com>
2017-03-29 14:36:31 -0400

    parameterize the deploy script

--- esc_deploy.sh
+++ esc_deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-BUILD_NUMBER=$1
-echo "Deploying build number $BUILD_NUMBER"
-
-SERVICE_NAME="flask-signup-service"
+SERVICE_NAME=$1
+BUILD_NUMBER=$2
+TASK_FAMILY=$3
+CLUSTER=${4:-default}
 IMAGE_VERSION="v_"${BUILD_NUMBER}
-TASK_FAMILY="flask-signup"
+
+echo "Deploying build number $BUILD_NUMBER"
 
 # Create a new task definition for this build
 sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" flask-signup.json > flask-signup-v_${BUILD_NUMBER}.json
 aws ecs register-task-definition --family flask-signup --cli-input-json file://flask-signup-v_${BUILD_NUMBER}.json
 
 # Update the service with the new task definition and desired count
-TASK_REVISION=`aws ecs describe-task-definition --task-definition flask-signup | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+TASK_REVISION=`aws ecs describe-task-definition --task-definition $TASK_FAMILY | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
 if [ ${DESIRED_COUNT} = "0" ]; then
     DESIRED_COUNT="1"
