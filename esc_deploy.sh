21df6638fc9bc5f838793071557d1003bae92032
Mat Pataki <matpataki@gmail.com>
2017-03-29 14:41:11 -0400

    use TASK_NAME throughout the script

--- esc_deploy.sh
+++ esc_deploy.sh
@@ -6,11 +6,11 @@ TASK_FAMILY=$3
 CLUSTER=${4:-default}
 IMAGE_VERSION="v_"${BUILD_NUMBER}
 
-echo "Deploying build number $BUILD_NUMBER"
+echo "Deploying build number $BUILD_NUMBER for service $SERVICE_NAME"
 
 # Create a new task definition for this build
-sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" flask-signup.json > flask-signup-v_${BUILD_NUMBER}.json
-aws ecs register-task-definition --family flask-signup --cli-input-json file://flask-signup-v_${BUILD_NUMBER}.json
+sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" $TASK_FAMILY.json > $TASK_FAMILY-v_${BUILD_NUMBER}.json
+aws ecs register-task-definition --family $TASK_FAMILY --cli-input-json file://$TASK_FAMILY-v_${BUILD_NUMBER}.json
 
 # Update the service with the new task definition and desired count
 TASK_REVISION=`aws ecs describe-task-definition --task-definition $TASK_FAMILY | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
