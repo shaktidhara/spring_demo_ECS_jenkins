e9ac6fc6870e05ecae95db77c9a287a173fa0adc
Mat Pataki <matpataki@gmail.com>
2017-03-30 15:57:06 -0400

    some debug help

--- ecs/deploy.sh
+++ ecs/deploy.sh
@@ -17,6 +17,7 @@ aws ecs register-task-definition --family $TASK_FAMILY --region 'us-east-1' --cl
 
 # Create the service if it doesn't already exist
 if aws ecs describe-services --region 'us-east-1' --services $SERVICE_NAME | jq -e .failures[0]; then
+  echo "aws ecs create-service --cluster $CLUSTER --region 'us-east-1' --service-name $SERVICE_NAME --task-definition $TASK_FAMILY --load-balancers loadBalancerName=$LOAD_BALANCER_NAME,containerName=$TASK_FAMILY,containerPort=$CONTAINER_PORT --role $ECS_SERVICE_ROLE --desired-count 0"
   aws ecs create-service --cluster $CLUSTER --region 'us-east-1' --service-name $SERVICE_NAME --task-definition $TASK_FAMILY --load-balancers loadBalancerName=$LOAD_BALANCER_NAME,containerName=$TASK_FAMILY,containerPort=$CONTAINER_PORT --role $ECS_SERVICE_ROLE --desired-count 0
 fi
 
