c48611da0327faae280d5e62b039e7c5650978e9
Gihad Fauaz Murad <gihad@uken.com>
2017-04-05 11:07:27 -0400

    Added actuator and 1 custom metric

--- src/main/java/com/platform/ApplicationController.java
+++ src/main/java/com/platform/ApplicationController.java
@@ -2,7 +2,9 @@ package com.platform;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +20,14 @@ public class ApplicationController {
 	
     @Value("${hello_response}")
     private String response;
+    
+    @Autowired
+    private CounterService counterService;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-    	logger.debug("Saying hello debug");
     	logger.info("Saying hello info");
+    	counterService.increment("demo.hello.request");
         return "Hi " + response + "!";
     }
 }
