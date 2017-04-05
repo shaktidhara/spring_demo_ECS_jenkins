0fdf2b5de8c20c8fa7a700b4153249b53c76292a
Gihad Fauaz Murad <gihad@uken.com>
2017-04-05 15:57:01 -0400

    metric export to cloudwatch using dropwizard

--- src/main/java/com/platform/MetricsConfig.java
+++ src/main/java/com/platform/MetricsConfig.java
@@ -1,25 +1,72 @@
 package com.platform;
 
-import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
-import org.springframework.boot.actuate.metrics.export.MetricExportProperties;
-import org.springframework.boot.actuate.metrics.writer.MetricWriter;
-import org.springframework.cloud.aws.actuate.metrics.BufferingCloudWatchMetricSender;
-import org.springframework.cloud.aws.actuate.metrics.CloudWatchMetricWriter;
-import org.springframework.context.annotation.Bean;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.codec.binary.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.endpoint.SystemPublicMetrics;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchAsync;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchAsyncClient;
+import com.blacklocus.metrics.CloudWatchReporter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.MetricSet;
+import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
+import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
 
-public class MetricsConfig {
+@Configuration
+@EnableMetrics
+public class MetricsConfig extends MetricsConfigurerAdapter {
 
-	private String prefix = "test";
-	private int maxBuffer = 1000;
-	private long fixedDelayBetweenRuns = 5000;
-	
+	final static Logger logger = LoggerFactory.getLogger(MetricsConfig.class);
 
-	@Bean
-	@ExportMetricWriter
-	MetricWriter metricWriter(MetricExportProperties export) {
-	    return new CloudWatchMetricWriter(new BufferingCloudWatchMetricSender(prefix, maxBuffer, fixedDelayBetweenRuns, new AmazonCloudWatchAsyncClient()));
+	@Value("${spring.application.name:demo2}")
+	private String applicationName;
+
+	@Value("${spring.profiles.active:dev}")
+	private String environment;
+
+	@Autowired
+	private SystemPublicMetrics systemPublicMetrics;
+
+	@Override
+	public void configureReporters(MetricRegistry metricRegistry) {
+		AmazonCloudWatchAsync amazonCloudWatchAsync = new AmazonCloudWatchAsyncClient();
+		amazonCloudWatchAsync.setRegion(Region.getRegion(Regions.US_EAST_1));
+
+		metricRegistry.register("spring.boot", (MetricSet) () -> {
+			final Map<String, Metric> gauges = new HashMap<String, Metric>();
+
+			for (final org.springframework.boot.actuate.metrics.Metric<?> springMetric : systemPublicMetrics
+					.metrics()) {
+
+				gauges.put(springMetric.getName(), (Gauge<Object>) () -> {
+
+					return systemPublicMetrics.metrics().stream()
+							.filter(m -> StringUtils.equals(m.getName(), springMetric.getName())).map(m -> m.getValue())
+							.findFirst().orElse(null);
+
+				});
+			}
+			return Collections.unmodifiableMap(gauges);
+		});
+
+		String nameSpace = environment + "." + applicationName;
+		CloudWatchReporter cloudWatchReporter = new CloudWatchReporter(metricRegistry, nameSpace, amazonCloudWatchAsync);
+
+		logger.info("Namespace is" + nameSpace);
+
+		registerReporter(cloudWatchReporter).start(1, TimeUnit.MINUTES);
 	}
-	
 }
