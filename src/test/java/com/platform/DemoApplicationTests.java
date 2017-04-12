50551ed5616084eb0a5e9a5689744599b39eef16
Gihad Fauaz Murad <gihad@uken.com>
2017-04-12 10:20:58 -0400

    removed useless test

--- src/test/java/com/platform/DemoApplicationTests.java
+++ src/test/java/com/platform/DemoApplicationTests.java
@@ -1,16 +1,14 @@
 package com.platform;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertTrue;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.junit.Test;
+
 public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
 	}
 
 }
