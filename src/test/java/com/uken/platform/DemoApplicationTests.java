fd444b7eb843624e65f532a67f01ff495f222f97
Mat Pataki <matpataki@gmail.com>
2018-02-01 19:09:05 -0500

    simulate long running tests

--- src/test/java/com/uken/platform/DemoApplicationTests.java
+++ src/test/java/com/uken/platform/DemoApplicationTests.java
@@ -3,11 +3,18 @@ package com.uken.platform;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 public class DemoApplicationTests {
+	Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws InterruptedException {
+	    logger.info("Simulating long running tests...");
+		TimeUnit.SECONDS.sleep(20);
 		assertTrue(true);
 	}
 
