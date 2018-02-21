ad659096ead49d098ae7f71c9e33a29d2293ce07
Mat Pataki <matpataki@gmail.com>
2018-02-21 15:35:13 -0500

    apply code formatter :troll:

--- src/main/java/com/uken/platform/DemoApplication.java
+++ src/main/java/com/uken/platform/DemoApplication.java
@@ -3,27 +3,25 @@ package com.uken.platform;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.UUID;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
-	
-	@Bean
-	public String hostname() {
-		String hostname;
-		try {
-			hostname = InetAddress.getLocalHost().getHostName();
-		} catch (UnknownHostException e) {
-			hostname = UUID.randomUUID().toString();
-		}
-        return hostname;
-    }
 
-	
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  @Bean
+  public String hostname() {
+    String hostname;
+    try {
+      hostname = InetAddress.getLocalHost().getHostName();
+    } catch (UnknownHostException e) {
+      hostname = UUID.randomUUID().toString();
+    }
+    return hostname;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 }
